a24be52fd197a5f937e321704e05c75a47021e10
alexung93 <alexung93@gmail.com>
2015-12-06 14:42:11 -0800

    updated functionality + style

--- test.php
+++ test.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>League</title>
+        <title>Draft</title>
         <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
         <meta content="utf-8" http-equiv="encoding">
         <style type="text/css">
@@ -13,43 +13,42 @@
         <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.10/css/dataTables.bootstrap.min.css"/>
         <link rel="stylesheet" type="text/css" href="css/history_style.css"/>
 
+
         <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.4.js"></script>
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.js"></script>
         <script type="text/javascript" src="https://cdn.datatables.net/1.10.10/js/jquery.dataTables.js"></script>
         <script type="text/javascript" src="https://cdn.datatables.net/1.10.10/js/dataTables.jqueryui.js"></script>
         <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
         <script type="text/javascript" src="https://cdn.datatables.net/1.10.10/js/dataTables.bootstrap.min.js"></script>
-        
         <link href="css/navbar.css" rel="stylesheet">
     </head>
-    <?php include 'header2.php'; ?>
 
+    <?php include 'header2.php'; ?>
     <body>
-        
          <?php
     
     $username = "root";
       $password = "";
       $host = "localhost";
 
-    //    $connector = mysql_connect($host,$username,$password)
-    //        or die("Unable to connect");
-    //      echo "Connections are made successfully::";
-    //    $selected = mysql_select_db("football", $connector)
+    //  $connector = mysql_connect($host,$username,$password)
     //      or die("Unable to connect");
-     //    $result = mysql_query("SELECT * FROM leagues");
+   //     echo "Connections are made successfully::";
+   //   $selected = mysql_select_db("fantasyfootball", $connector)
+   //     or die("Unable to connect");
+  //    $result = mysql_query("SELECT title, entries, prize FROM leagues");
 
         ?>
 
         <section class="main">
-          <div class="container">
+          <div class="container main-container">
             <div class="fill-draft">
                 <div class="row">
                 </div>
              </div>
           </div>
         </section>
-        
+
 <div class="container">
     <div class="row">
             <div class="col-lg-12">
@@ -61,12 +60,12 @@
                 </ol>     
             </div>
     </div>
+
+<div class="row">
+<div class="col-lg-12">
+    <h1 align="center">My Lineups</h1>
+    <hr class = "style1">
     
-    <div class="row">
-        <div class="col-lg-12">
-            <h1 align="center">My Lineups</h1>
-            <hr>
-        
         <div class="panel panel-primary filterable">
             <div class="panel-heading">
                 <h3 class="panel-title" align="right">Press the filter button below to filter your search</h3>
@@ -83,58 +82,34 @@
                         <th><input type="text" class="form-control" placeholder="Payout" disabled></th>
                     </tr>
                 </thead>
-                <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>7000</td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>5000</td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td>Joey</td>
-                        <td>the Bird</td>
-                        <td>2000</td>
+        
                     </tr>
                 </tbody>
                  <tbody>
                 
-        <?php
-     //     while( $row = mysql_fetch_assoc( $result ) ){
-      //      echo
-     //       "<tr>
-      //        <td>{$row\['profile_id'\]}</td>
-     //         <td>{$row\['prize'\]}</td>
-     //       
-     //       </tr>\n";
-     //     }
+          <?php
+          $a = 1;
+          while( $row = mysql_fetch_assoc( $result ) ){
+    echo "<tr>";
+    echo "<td>" . $a . "</td>";
+    echo "<td>" . $row['title'] . "</td>";
+    echo "<td>" . $row['entries'] . "</td>";
+    echo "<td>" . $row['prize'] . "</td>";
+   
+    echo "</tr>\n";
+    $a++;
+          }
         ?>
 
-                </tbody>
+      </tbody>
             </table>
-
-          </div>
-
         </div>
-
     </div>
-   
-
-
 </div>
- <?php include 'footer.php'; ?>
 
+<? php include 'footer.php'; ?>
 
-
-    
 </body>
-
 
 
 <script type="text/javascript">
